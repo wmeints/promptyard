7beae7b19b1b6904c7e36f6f346d6b19536ad52c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-11-14 07:05:48 +0100

    feat: migrate to using allauth

--- core/urls.py
+++ core/urls.py
@@ -17,13 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("accounts/password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("accounts/reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("accounts/reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("accounts/", include("allauth.urls")),
 ]
