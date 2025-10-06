d6de2629239b543802b172294aa368bb7d4fe68d
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-10-06 07:39:50 +0200

    Add authentication support

---
+++ server/src/auth.ts
@@ -0,0 +1,12 @@
+import { db } from '@/db';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true
+    },
+    database: drizzleAdapter(db, {
+        provider: "pg"
+    })
+});
