6c2a50913c657f989a23904cf586d1d677fde9ab
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-12-28 10:25:14 +0100

    Add database to the application host

--- host/apphost.cs
+++ host/apphost.cs
@@ -1,10 +1,16 @@
-﻿#:package CommunityToolkit.Aspire.Hosting.Bun@13.0.1-beta.468
+﻿#:package Aspire.Hosting.PostgreSQL@13.1.0
+#:package CommunityToolkit.Aspire.Hosting.Bun@13.0.1-beta.468
 #:sdk Aspire.AppHost.Sdk@13.1.0
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var databaseServer = builder.AddPostgres("postgres").WithDataVolume();
+var applicationDatabase = databaseServer.AddDatabase("database", "promptyard");
+
 builder.AddBunApp("portal", "../portal", "dev")
+    .WithReference(applicationDatabase)
     .WithHttpEndpoint(port: 3000, env: "PORT")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WaitFor(applicationDatabase);
 
 builder.Build().Run();
