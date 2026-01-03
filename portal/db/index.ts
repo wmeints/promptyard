93166cf8ff7204f99b37ec2727d0ea5e528aa9b9
Willem Meints <1550763+wmeints@users.noreply.github.com>
2026-01-03 16:17:26 +0100

    Remove packages and configuration for database (#98)

--- portal/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
-
-// Note: The DATABASE_URI is automatically configured by Aspire.
-// If you're not using Aspire, make sure you set the DATABASE_URI environment
-// variable or store it in a .env file in this directory.
-export const db = drizzle(process.env.DATABASE_URI!, { schema });
